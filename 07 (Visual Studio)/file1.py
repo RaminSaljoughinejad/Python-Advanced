adb8bd9f620d7012a8d2a77919e27341c1427d01
Ramin Saljoughinejad <37393787+RaminSaljoughinejad@users.noreply.github.com>
2022-04-20 13:47:43 +0430

    second commit

---
+++ 07 (Visual Studio)/file1.py
@@ -0,0 +1,17 @@
+from file2 import *
+
+instance = A() 
+
+print(instance.__dict__)
+
+print(instance.val)
+print(instance._val)
+print(instance._A__val)
+
+
+####
+#print(my_var)
+####
+#print(_private)
+####
+#print(__private)
---
+++ 07 (Visual Studio)/file2.py
@@ -0,0 +1,15 @@
+class A:
+    def __init__(self):
+        self.val = 2
+        self._val = 3
+        self.__val = 4
+
+
+
+
+
+"""my_var = 10
+
+_private = 2
+
+__private = 3"""
